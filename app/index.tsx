314e547624bff90efabc2d077110aa1b849f7da5
ktcotz <naskret.kamil@gmail.com>
2025-04-22 14:58:16 +0200

    feat: update expo intellisense
    Update expo, intellisense

--- app/index.tsx
+++ app/index.tsx
@@ -3,9 +3,7 @@ import { Text, View } from 'react-native';
 export default function Index() {
 	return (
 		<View className='bg-black min-h-screen flex items-center justify-center'>
-			<Text className='text-red-500 text-5xl font-bold leading-tight'>
-				Witaj świecie ReactNative!
-			</Text>
+			<Text className='text-red-500 text-5xl font-bold leading-tight'>Witaj świecie!</Text>
 		</View>
 	);
 }
